2a49ff7f31b14cb58409818992357f1ca0e4bb98
Bilal Elakry <bilal.elakry@emsi-edu.ma>
2026-09-03 22:45:50 +0100

    Initial commit: configuration Spring Boot et premier endpoint

---
+++ src/main/java/com/smaservices/publication_api/PublicationApiApplication.java
@@ -0,0 +1,13 @@
+package com.smaservices.publication_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smaservices/publication_api/PublicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smaservices.publication_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
